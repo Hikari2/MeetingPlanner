4d1d777d63a659ac3e2bd9dd1bfae00cd11895b0
Jiansun <jians@kth.se>
2016-04-08 15:18:21 +0200

    improve the footer and login

--- app/controllers/authenticationCtrl.js
+++ app/controllers/authenticationCtrl.js
@@ -17,15 +17,19 @@ meetingAgendaBuilder.controller('AuthenticationCtrl', function ($scope, $locatio
         }).catch(function (error) {
             if (error.code == "INVALID_EMAIL") {
                 $scope.status = "The    specified user account email is invalid.";
+                document.getElementById("buttonreplacement").style.display = "none";
             }
             else if (error.code == "INVALID_PASSWORD") {
                 $scope.status = "The specified user account password is incorrect.";
+                document.getElementById("buttonreplacement").style.display = "none";
             }
             else if (error.code == "INVALID_USER") {
                 $scope.status = "The specified user account does not exist.";
+                document.getElementById("buttonreplacement").style.display = "none";
             }
             else {
                 $scope.status = "Something went wrong";
+                document.getElementById("buttonreplacement").style.display = "none";
             }
         });
     }
@@ -45,12 +49,15 @@ meetingAgendaBuilder.controller('AuthenticationCtrl', function ($scope, $locatio
         }).catch(function (error) {
             if (error.code == "EMAIL_TAKEN") {
                 $scope.status = "The new user account cannot be created because the email is already in use.";
+                document.getElementById("buttonreplacement").style.display = "none";
             }
             else if (error.code == "INVALID_EMAIL") {
                 $scope.status = "The specified email is not a valid email.";
+                document.getElementById("buttonreplacement").style.display = "none";
             }
             else {
                 $scope.status = "Something went wrong";
+                document.getElementById("buttonreplacement").style.display = "none";
             }
         });
     }
