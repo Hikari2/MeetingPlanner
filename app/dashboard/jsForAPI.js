4777dc3d87d823c9f7a6d0065f931e2dead6783a
Jiansun <jians@kth.se>
2016-04-24 23:25:10 +0200

    map works

---
+++ app/dashboard/jsForAPI.js
@@ -0,0 +1,39 @@
+/**
+ * Created by sunxuan on 4/24/16.
+ */
+
+    function init()
+    {
+//            timedMsg();
+        // get the current location
+        navigator.geolocation.getCurrentPosition(function(position)
+        {
+            var coords = position.coords;
+            // set the lat and lng
+            var latlng = new google.maps.LatLng(coords.latitude,coords.longitude);
+            var myOptions =
+            {
+                // zoom in
+                zoom:14,
+                // lat and lng
+                center:latlng,
+                // map type
+                mapTypeId:google.maps.MapTypeId.ROADMAP
+            };
+            var map1;
+            // show map
+            map1 = new google.maps.Map(document.getElementById('map'),myOptions);
+            // build marker
+            var marker = new google.maps.Marker(
+                {
+                    position:latlng,
+                    map:map1
+                });
+            var infowindow = new google.maps.InfoWindow(
+                {
+                    content:"Your Location!"
+                });
+            // pop up
+            infowindow.open(map1,marker);
+        });
+    }
